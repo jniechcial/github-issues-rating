32e3550f58f6baebcdef6ce113b97ac49512ab9b
Jakub Niechcial <jakub.niechcial@intercom.io>
2018-04-14 23:38:58 +0100

    Add option for manual bumps

--- issue_rater.rb
+++ issue_rater.rb
@@ -1,16 +1,33 @@
 class IssueRater
   WINDOW_TIME_DIFFERENCE = 365
   WINDOW_LOW_RESULT = 0.2
+  MANUAL_BUMPS_PATH = './manual_bumps.bak'
+
+  def self.manual_bumps
+    return @manual_bumps if @manual_bumps
+    @manual_bumps = {}
+
+    CSV.foreach(MANUAL_BUMPS_PATH) do |row|
+      issue_number, bump = row[0].split(' | ')
+      @manual_bumps[issue_number] = bump.to_f
+    end
+
+    @manual_bumps
+  end
 
   def initialize(issue:)
     @issue = issue
   end
 
   def calculate_rating
-    (rate_customer_logs_count + rate_system_priority + rate_vip_customer) * time_window_multiplier
+    (rate_customer_logs_count + rate_system_priority + rate_vip_customer) * time_window_multiplier + manual_bump
   end
 
   private
+
+  def manual_bump
+    IssueRater.manual_bumps[@issue.number] || 0
+  end
 
   def rate_customer_logs_count
     @issue.comments.select { |comment| comment.is_customer_log? }.length
